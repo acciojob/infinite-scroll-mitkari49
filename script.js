94caae5dcb93d2acce5b724b49f4da5275eb4448
mitkari49 <93242478+mitkari49@users.noreply.github.com>
2024-09-05 23:00:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 //your code here!
+// first add 10 elements
+// add elements +2 after each scoll reached the end
+let itemCount=10;
+const list=document.getElementById("infi-list");
+// loading item's 
+function loadElements(){
+	for(let i=0;i<2;i++){
+		const newItem=document.createElement("li");
+		itemCount++;
+		newItem.innerHTML=`item ${itemCount}`
+		list.appendChild(newItem);
+	}
+}
 
+function End (){
+	return list.scrollTop + list.clientHeight >= list.scrollHeight;
+}
+
+list.addEventListener('scroll',()=>{
+	if (End()){
+		loadElements();
+	}
+});
